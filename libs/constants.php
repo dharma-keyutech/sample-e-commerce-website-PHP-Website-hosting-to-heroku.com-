2dd0fe60f657471a9c72f70b44fbc1e05aaba299
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-05-21 20:07:11 +0530

    Update constants.php

--- libs/constants.php
+++ libs/constants.php
@@ -15,12 +15,11 @@ define('SITE_DIR', '/simple-website/');
 // database prefix if you use
 define('DB_PREFIX', 'mp_');
 
-define('DB_DRIVER', 'mysql');
-define('DB_HOST', 'localhost');
-define('DB_HOST_USERNAME', 'root');
-define('DB_HOST_PASSWORD', '');
+define('DB_DRIVER', 'd93o93c07vsmp5');
+define('DB_HOST', 'ec2-35-170-85-206.compute-1.amazonaws.com');
+define('DB_HOST_USERNAME', 'ffxbgbrdudwwrc');
+define('DB_HOST_PASSWORD', 'dce41c0c5ceaa4458bfe68998867ca582a5952935c9be96aa7223455d5e8e7e8');
 define('DB_DATABASE', 'simple-website');
-
 define('SITE_NAME', 'Simple Website');
 
 // define database tables
