a569eab65bd254c52a5bc6b8febfb52164328269
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-05-21 20:13:19 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE `simple-website`;
-USE `simple-website`;
+CREATE DATABASE `sample-php-website`;
+USE `sample-php-website`;
 
 CREATE TABLE IF NOT EXISTS `mp_pages` (
   `page_id` int(11) NOT NULL AUTO_INCREMENT,
