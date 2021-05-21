90329d6d496b8ae8c69907e78c86ac1501d131c2
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-05-21 20:53:12 +0530

    Update config.php

--- libs/config.php
+++ libs/config.php
@@ -36,7 +36,7 @@ $dboptions = array(
     PDO::ATTR_PERSISTENT => FALSE,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::PostgreSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    PDO::MySQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 );
 
 //connect with the server
